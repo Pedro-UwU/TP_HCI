92cfc10cdaf616c499ec4b94e1779f252efdf831
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-04-26 20:18:12 -0300

    vuetify header

---
+++ fitnice/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+
+    <v-app-bar
+        color="indigo lighten-1"
+        dense
+        dark
+    >
+      <v-toolbar-title>
+        Fitnice
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        
+<!--        <router-link class="white&#45;&#45;text" to="/">Home</router-link>-->
+      </v-btn>
+<!--      <v-btn text>-->
+<!--        <router-link class="white&#45;&#45;text" to="/Contact">Contact</router-link>-->
+<!--      </v-btn>-->
+<!--      <v-btn text>-->
+<!--        <router-link class="white&#45;&#45;text" to="/About">About</router-link>-->
+<!--      </v-btn>-->
+
+    </v-app-bar>
+  </div>
+</template>
+
+<style scoped>
+span a {
+  text-decoration: none;
+}
+</style>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+
---
+++ fitnice/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <h1>Home</h1>
+  </v-app>
+</template>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
+
+<script>
+
+export default {
+  name: "Home",
+
+  components: {
+
+  }
+}
+</script>
