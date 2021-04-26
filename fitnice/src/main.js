d4d7734f4d6e7eb049410552e6ab99a0697a6ec7
Pedro <pelopez@itba.edu.ar>
2021-04-26 20:59:49 -0300

    Implementacion del Router

--- fitnice/src/main.js
+++ fitnice/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes:[
+    { path:'/', component: Home },
+    { path: '*', component: NotFound }
+  ]
+});
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
