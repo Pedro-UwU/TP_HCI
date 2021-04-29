9f16c9bee344c14b892ab6667948a974a7dc257d
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-04-29 13:50:37 -0300

    header complete

--- fitnice/src/components/Header.vue
+++ fitnice/src/components/Header.vue
@@ -1,29 +1,43 @@
 <template>
   <div>
-
     <v-app-bar
-        color="indigo lighten-1"
+        color="#374140"
         dense
         dark
+        height="70px"
     >
-      <v-toolbar-title>
-        Fitnice
+      <v-toolbar-title style="margin-left: 30px; margin-top:20px">
+        <img src="../assets/Fitnice.svg" height="50px" width="auto" alt="logo">
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn text>
-        
-<!--        <router-link class="white&#45;&#45;text" to="/">Home</router-link>-->
+      <v-btn icon color="white">
+        <img src="../assets/lupa.svg" alt="lupa icon"/>
       </v-btn>
-<!--      <v-btn text>-->
-<!--        <router-link class="white&#45;&#45;text" to="/Contact">Contact</router-link>-->
-<!--      </v-btn>-->
-<!--      <v-btn text>-->
-<!--        <router-link class="white&#45;&#45;text" to="/About">About</router-link>-->
-<!--      </v-btn>-->
+      <v-btn icon color="white" style="margin: 10px">
+        <img src="../assets/person.svg" alt="person icon"/>
+      </v-btn>
 
     </v-app-bar>
+    <template id="nav">
+    <v-tabs
+        fixed-tabs
+        color="#5885B3"
+        background-color="#141D26"
+        dark
+    >
+      <v-tab>
+        Inicio
+      </v-tab>
+      <v-tab>
+        Rutinas
+      </v-tab>
+      <v-tab>
+        Favoritos
+      </v-tab>
+    </v-tabs>
+</template>
   </div>
 </template>
 
@@ -34,8 +48,18 @@ span a {
 </style>
 
 <script>
+import Vuetify from "vuetify";
+
+  var nav = new Vuetify({
+    el: nav,
+    data : {
+      navigation : ["Inicio","Rutinas","Favoritos"],
+      color: "#374140"
+    }
+  });
 export default {
   name: "Header"
+
 }
 </script>
 
