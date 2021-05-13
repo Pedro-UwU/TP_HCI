2f420758b0571be28a0054d086fac8c7e7acf648
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-05-13 20:27:11 -0300

    Revert "Merge branch 'main' of https://github.com/PedroLopezITBA/TP_HCI into main"
    This reverts commit 3cac724cfd0a853cbdc97669f68c9c228dd36d8c, reversing
    changes made to b0c96418282b8a0e87cc3fee480dcf7a4cb10094.

--- fitnice/src/store/Cycle.js
+++ fitnice/src/store/Cycle.js
@@ -1,20 +1,12 @@
-export default class Cycle{
-    // static idCount = 0;
-    // constructor(name, repetitions) {
-    //     super();
-    //     this.id = Cycle.idCount;
-    //     Cycle.idCount++;
-    //     this.name = name;
-    //     this.repetitions = repetitions;
-    // }
+import ExerciseStore from "./ExerciseStore";
 
-    constructor(name, detail, type, order, repetitions, routineId, id) {
+export default class Cycle extends ExerciseStore{
+    static idCount = 0;
+    constructor(name, repetitions) {
+        super();
+        this.id = Cycle.idCount;
+        Cycle.idCount++;
         this.name = name;
-        this.detail = detail;
-        this.type = type;
-        this.order = order;
         this.repetitions = repetitions;
-        this.routineId = routineId;
-        this.id = id;
     }
 }
