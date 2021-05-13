ba80141e708dc79d68275fc358023b1d98f3a467
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-05-13 20:41:31 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/PedroLopezITBA/TP_HCI into main""
    This reverts commit 2f420758b0571be28a0054d086fac8c7e7acf648.

--- fitnice/src/store/Routine.js
+++ fitnice/src/store/Routine.js
@@ -1,30 +1,41 @@
 export default class Routine {
-    static idCount = 0;
-    constructor(name, category, difficulty, duration) {
-        this.id = Routine.idCount;
-        Routine.idCount++;
+    // static idCount = 0;
+    // constructor(name, category, difficulty, duration) {
+    //     this.id = Routine.idCount;
+    //     Routine.idCount++;
+    //     this.name = name;
+    //     this.category = category;
+    //     this.difficulty = difficulty;
+    //     this.duration =  duration;
+    //     this.cycles = [];
+    //     this.isPublic = true;
+    //     this.description = '';
+    // }
+    //
+    // // constructor(name, detail, isPublic, ) {
+    // // }
+    //
+    // addCycle(cycle) {
+    //     this.cycles.push(cycle);
+    // }
+    //
+    // removeCycle(cycle) {
+    //     this.cycles.splice(this.cycles.indexOf(cycle), 1);
+    // }
+    //
+    // setDescription(desc) {
+    //     this.description = desc;
+    // }
+    //
+    // getIDCount() {
+    //     return Routine.idCount
+    // }
+    constructor(name, detail, isPublic, difficulty, category, id) {
+        this.id = id;
         this.name = name;
-        this.category = category;
+        this.detail = detail;
+        this.isPublic = isPublic;
         this.difficulty = difficulty;
-        this.duration =  duration;
-        this.cycles = [];
-        this.isPublic = true;
-        this.description = '';
-    }
-
-    addCycle(cycle) {
-        this.cycles.push(cycle);
-    }
-
-    removeCycle(cycle) {
-        this.cycles.splice(this.cycles.indexOf(cycle), 1);
-    }
-
-    setDescription(desc) {
-        this.description = desc;
-    }
-
-    getIDCount() {
-        return Routine.idCount
+        this.category = category;
     }
 }
